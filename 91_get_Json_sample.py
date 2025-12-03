5a90fc211f2de9552414054124614cbd2545e632
Daniel Vilas <dvilas@csic.es>
2025-12-03 20:11:05 +0100

    Hasta 1963

--- 91_get_Json_sample.py
+++ 91_get_Json_sample.py
@@ -47,54 +47,94 @@ pdfs_to_process = [
     ("hoy",1940,4,0,11,None,False), # Columnas
     ("extremadura",1940,5,10,2,None,True), # Sequia
     ("extremadura",1940,5,10,4,None,False), # Texto
-
     ("hoy",1941,11,15,1,"BAD",False), # Portada
     ("hoy",1941,11,15,3,"BAD",True), # Sequia
+    ("extremadura",1941,11,25,4,None,True), # Sequia
+    ("extremadura",1941,11,25,1,None,False), # Portada
     ("hoy",1942,10,20,1,"BAD",False), # Portada
     ("hoy",1942,10,20,7,"BAD",False), # Columnas
+    ("extremadura",1942,8,7,1,None,False), # Portada
     ("hoy",1943,4,17,6,"CAC",True), # Sequia
     ("hoy",1943,4,17,1,"CAC",False), # Portada
+    ("extremadura",1943,7,7,1,None,False), # Portada
+    ("extremadura",1943,7,7,3,None,True), # Sequia
     ("hoy",1944,3,5,5,"BAD",False), # Columnas
     ("hoy",1944,3,5,4,"MER",True), # Rogativa
+    ("extremadura",1944,4,24,2,None,False), # Columnas
+    ("extremadura",1944,4,24,3,None,True), # Sequia
     ("hoy",1945,7,18,3,"BAD",False), # Texto Dificil
     ("hoy",1945,7,18,7,"BAD",True), # Sequia
+    ("extremadura",1945,8,27,1,None,False), # Portada
+    ("extremadura",1945,8,27,4,None,True), # Sequia
     ("hoy",1946,8,18,1,"BAD",False), # Portada
     ("hoy",1946,8,18,4,"BAD",True), # Sequia
+    ("extremadura",1946,8,5,4,None,False), # Texto
+    ("extremadura",1946,8,5,3,None,True), # Sequia
     ("hoy",1947,5,1,4,"CAC",False), # Anuncios
     ("hoy",1947,5,1,3,"BAD",True), # Sequia
+    ("extremadura",1947,5,5,1,None,False), # Portada
+    ("extremadura",1947,5,5,3,None,True), # Sequia
     ("hoy",1948,12,1,4,"CAC",False), # Fotos
     ("hoy",1948,12,1,3,"BAD",True), # Sequia
+    ("extremadura",1948,12,18,4,None,False), # Columnas
+    ("extremadura",1948,12,18,3,None,True), # Sequia
     ("hoy",1949,6,15,1,"BAD",False), # Portada
     ("hoy",1949,6,15,4,"BAD",True), # Sequia
+    ("extremadura",1949,8,1,1,None,False), # Portada
+    ("extremadura",1949,8,1,3,None,True), # Sequia
     ("hoy",1950,9,21,2,"BAD",False), # Columnas
     ("hoy",1950,9,21,5,"BAD",True), # Sequia
+    ("extremadura",1950,9,9,1,None,False), # Portada
+    ("extremadura",1950,9,9,4,None,True), # Sequia
     ("hoy",1951,1,17,6,"BAD",False), # Fotos
     ("hoy",1951,1,17,4,"CAC",True), # Sequia
+    ("extremadura",1951,2,3,3,None,False), # Texto
     ("hoy",1952,11,18,3,"BAD",False), # Tabla
     ("hoy",1952,11,18,4,"CAC",True), # Sequia
     ("hoy",1952,11,18,6,"BAD",False), # Anuncio pal Sequia
+    ("extremadura",1952,11,12,1,None,False), # Portada
+    ("extremadura",1952,11,12,4,None,True), # Sequia
     ("hoy",1953,8,30,1,"BAD",False), # Portada
     ("hoy",1953,8,30,4,"CAC",True), # Sequia
+    ("extremadura",1953,9,1,1,None,True), # Sequia
     ("hoy",1954,3,4,2,"BAD",False), # Columnas y textos cortos
     ("hoy",1954,3,4,5,"BAD",True), # Sequia
+    ("extremadura",1954,10,29,5,None,False), # Fotos
+    ("extremadura",1954,10,29,2,None,True), # Sequia
     ("hoy",1955,10,25,1,"CAC",False), # Portada
     ("hoy",1955,10,25,2,"BAD",True), # Sequia 
+    ("extremadura",1955,11,10,1,None,False), # Portada
+    ("extremadura",1955,11,10,3,None,True), # Sequia
     ("hoy",1956,2,14,2,"BAD",False), # Columnas
     ("hoy",1956,2,14,8,"BAD",False), # Nuevo diseño columnas
+    ("extremadura",1956,2,14,3,None,False), # Muchos Anuncios
     ("hoy",1957,4,25,1,"BAD",False), # Portada
     ("hoy",1957,4,25,6,"BAD",True), # Sequia (esta mal en el ods, dice 23)
+    ("extremadura",1957,12,10,2,None,False), # Texto
+    ("extremadura",1957,12,10,4,None,True), # Sequia
     ("hoy",1958,11,13,8,"CAC",False), # Columnas
     ("hoy",1958,11,13,6,"BAD",True), # Sequia 
+    ("extremadura",1958,11,14,1,None,False), # Nueva Portada
+    ("extremadura",1958,11,14,5,None,True), # Sequia
     ("hoy",1959,11,13,3,"BAD",False), # Columnas
     ("hoy",1959,11,13,6,"CAC",True), # Sequia 
+    ("extremadura",1959,3,5,4,None,False), # Texto
+    ("extremadura",1959,3,5,6,None,True), # Sequia
     ("hoy",1960,6,5,3,"BAD",False), # Columnas
     ("hoy",1960,6,5,1,"CAC",False), # Portada
+    ("extremadura",1960,5,27,1,None,False), # Portada
+    ("extremadura",1960,5,27,9,None,True), # Sequia
     ("hoy",1961,9,20,3,"BAD",False), # Columnas
     ("hoy",1961,9,20,2,"CAC",True), # Sequia 
+    ("extremadura",1961,9,20,5,None,False), # Tabla
     ("hoy",1962,3,17,3,"BAD",False), # Columnas
     ("hoy",1962,3,17,9,"BAD",True), # Sequia 
+    ("extremadura",1962,9,21,1,None,False), # Portada
+    ("extremadura",1962,9,21,4,None,True), # Sequia
     ("hoy",1963,10,6,16,"CAC",False), # Columnas
     ("hoy",1963,10,6,8,"CAC",True), # Sequia 
+    ("extremadura",1963,10,10,2,None,False), # Texto
+    ("extremadura",1963,10,10,3,None,True), # Sequia
 ]
 
 pdf_manager = PdfManager()
@@ -133,7 +173,20 @@ def main():
             
             json.dump(json_data,f,ensure_ascii=False,indent=4)
         print(f"Wrote JSON to {json_outfile}\n{'-'*40}\n")
-    pass
+    # gravamos un csv con los ficheros
+    with open("data/datasets/json_test/file_list.csv","w",encoding="utf-8") as f:
+        f.write("pdf_file,json_file,drought\n")
+        for pdf in pdfs_to_process:
+            paper, year, month, day, page, ed, flag = pdf
+            pdf_file=f"{paper}_{year}_{month}_{day}_page_{page}"
+            if ed:
+                pdf_file+=f"_{ed}"
+            pdf_file+=f".pdf"
+            json_file=f"{paper}_{year}_{month}_{day}_page_{page}"
+            if ed:
+                json_file+=f"_{ed}"
+            json_file+=f".json"
+            f.write(f"{pdf_file},{json_file},{flag}\n")
 
 if __name__ == "__main__":
     main()
