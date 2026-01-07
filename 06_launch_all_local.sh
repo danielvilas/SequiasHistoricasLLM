5a46cbc338cee6cac5b20216cce5f6ccbd195310
Daniel Vilas <dvilas@csic.es>
2026-01-07 14:55:32 +0100

    detec and classify

--- 06_launch_all_local.sh
+++ 06_launch_all_local.sh
@@ -21,5 +21,13 @@ python 06_launch_ciena.py full fastest-summary detect
 python 06_launch_ciena.py full efficient-no-summary detect
 python 06_launch_ciena.py full efficient-no-summary detect
 python 06_launch_ciena.py full efficient-summary detect
-python 06_launch_ciena.py full bestf1-no-summary detect
-python 06_launch_ciena.py full bestf1-summary detect
+#python 06_launch_ciena.py full bestf1-no-summary detect
+#python 06_launch_ciena.py full bestf1-summary detect
+
+python 06_launch_ciena.py full fastest-no-summary classify
+python 06_launch_ciena.py full fastest-summary classify
+python 06_launch_ciena.py full efficient-no-summary classify
+python 06_launch_ciena.py full efficient-no-summary classify
+python 06_launch_ciena.py full efficient-summary classify
+#python 06_launch_ciena.py full bestf1-no-summary classify
+#python 06_launch_ciena.py full bestf1-summary classify
